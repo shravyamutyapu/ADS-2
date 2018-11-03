b0d673645ed7520ebc96a6abee56c0d590d32ce8
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-11-03 12:19:31 +0530

    "ads2_m6_a1_1: 0/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 Assignments/M6/ADS-2Week-1/Solution.java
+++ ADS-2 Assignments/M6/ADS-2Week-1/Solution.java
@@ -1,6 +1,15 @@
 import java.util.Scanner;
 class PageRank {
-
+   Digraph dg;
+   int vertex;
+   double[] pageRankvals;
+   int[] outCount;
+   PageRank(Digraph dg1, int vt, double[] pageRankvals){
+   	this.dg = dg1;
+   	this.vertex = vt;
+   	pageRankvals = new double[vt];
+   	outCount = new int[vt];
+   }
 }
 
 class WebSearch {
@@ -14,14 +23,19 @@ public class Solution {
         Scanner sc = new Scanner(System.in);
 
         int vertexCount = Integer.parseInt(sc.nextLine());
+        Digraph digraph = new Digraph(vertexCount);
+
+        String[] incomingvertices = new String[vertexCount];
         for(int i = 0;i<vertexCount;i++){
           String[] tokens = sc.nextLine().split(" ");
           int key = Integer.parseInt(tokens[0]);
+          for(int j = 1; j<tokens.length; j++) {
+                digraph.addEdge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[i]));
 
-          int value = Integer.parseInt(tokens[1]);
-
+         }
 
         }
+        System.out.println(digraph);
 		// iterate count of vertices times
 		// to read the adjacency list from std input
 		// and build the graph
